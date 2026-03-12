8f0ee5c9bb08647d2b3a8550aaeafc12d7c5abdb
Oscaner Miao <oscaner1997@gmail.com>
2026-03-12 16:26:58 +0800

    feat: export log_context symbols and RequestContextFilter from utils

--- flow_proxy_plugin/utils/__init__.py
+++ flow_proxy_plugin/utils/__init__.py
@@ -1,20 +1,37 @@
 """Utility modules."""
 
+from .log_context import (
+    clear_request_context,
+    component_context,
+    get_request_prefix,
+    set_request_context,
+)
 from .log_filter import (
     BrokenPipeFilter,
     ProxyNoiseFilter,
     setup_proxy_log_filters,
 )
-from .logging import ColoredFormatter, Colors, setup_colored_logger, setup_logging
+from .logging import (
+    ColoredFormatter,
+    Colors,
+    RequestContextFilter,
+    setup_colored_logger,
+    setup_logging,
+)
 from .plugin_pool import PluginPool
 from .process_services import ProcessServices
 
 __all__ = [
+    "clear_request_context",
+    "component_context",
+    "get_request_prefix",
+    "set_request_context",
     "BrokenPipeFilter",
     "ProxyNoiseFilter",
     "setup_proxy_log_filters",
     "Colors",
     "ColoredFormatter",
+    "RequestContextFilter",
     "setup_colored_logger",
     "setup_logging",
     "PluginPool",
